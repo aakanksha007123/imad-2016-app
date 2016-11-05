06592e7cdf5aacd5cf80ecda4f58191a3e19f914
aakanksha007123 <aakanksha007123@gmail.com>
2016-11-05 15:16:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,21 +4,70 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
-
-
+var articles{
+  'article-one':{
+    title:'article one|Aakanksha Malhotra',
+    heading:'article-one',
+    content:`<p> This is the content of my first article. Hello my name is Aakanksha Malhotra.I am a Btech Student of 3rd year at JSS Academy of Technical Education,Noida ,Uttar Pradesh</p>
+          <p> This is the content of my first article. Hello my name is Aakanksha Malhotra.I am a Btech Student of 3rd year at JSS Academy of Technical Education,Noida ,Uttar Pradesh</p>
+          <p> This is the content of my first article. Hello my name is Aakanksha Malhotra.I am a Btech Student of 3rd year at JSS Academy of Technical Education,Noida ,Uttar Pradesh</p>`
+          },       
+'article-two':{
+              title:'article two|Aakanksha Malhotra',
+               heading:'article-two',
+               content:`<p> This is the content of my second article. Hello my name is Aakanksha Malhotra.I am a Btech Student of         3rd year at JSS Academy of Technical Education,Noida ,Uttar Pradesh</p>
+                        <p> This is the content of my second article. Hello my name is Aakanksha Malhotra.I am a Btech Student of 3rd year at JSS Academy of Technical Education,Noida ,Uttar Pradesh</p>
+                         <p> This is the content of my second article. Hello my name is Aakanksha Malhotra.I am a Btech Student of 3rd year at JSS Academy of Technical Education,Noida ,Uttar Pradesh</p>`
+    
+              },
+'article-three':{
+    title:'article three|Aakanksha Malhotra',
+    heading:'article-three',
+    content:`<p> This is the content of my third article. Hello my name is Aakanksha Malhotra.I am a Btech Student of 3rd year at JSS Academy of Technical Education,Noida ,Uttar Pradesh</p>
+          <p> This is the content of my third article. Hello my name is Aakanksha Malhotra.I am a Btech Student of 3rd year at JSS Academy of Technical Education,Noida ,Uttar Pradesh</p>
+          <p> This is the content of my third article. Hello my name is Aakanksha Malhotra.I am a Btech Student of 3rd year at JSS Academy of Technical Education,Noida ,Uttar Pradesh</p>`}
+};
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var contents=data.contents;
+}
+var htmlTemplate=`
+<html>
+<head>
+    <title> 
+         ${title}
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/ui/style.css" rel="stylesheet" />
+  </head>
+  <body>
+      <div>
+          <a href="\">Home</a>
+                ${heading}
+      </div>
+      <hr/>
+      <div>
+             ${date}
+      </div>
+      <div>
+             ${contents}
+          
+      </div>
+      </body>
+</html>
+`;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,res){
-res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/:articleName',function(req,res){
+    var articleName=req.params.articleName;
+res.send(createTemplate(articles[articleName]));
 });
-app.get('/article-two',function(req,res){
-res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three',function(req,res){
-res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -30,5 +79,5 @@ app.get('/ui/madi.png', function (req, res) {
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+  console.log(`IMAD course app listening on port ${port}!);
 });
