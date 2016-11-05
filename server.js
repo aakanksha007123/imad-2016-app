43f0597194993bd85e71b4b66cba2bab0738dcfd
aakanksha007123 <aakanksha007123@gmail.com>
2016-11-05 18:12:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,69 +4,12 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var articles{
-  'article-one':{
-                 title:'article one|Aakanksha Malhotra',
-                 heading:'article-one';
-                 date:November5 2016,
-                 content:`
-                  <p> This is the content of my first article. Hello my name is Aakanksha Malhotra.I am a Btech Student of 3rd year at JSS Academy of Technical Education,Noida ,Uttar Pradesh</p>
-                       <p> This is the content of my first article. Hello my name is Aakanksha Malhotra.I am a Btech Student of 3rd year at JSS Academy of Technical Education,Noida ,Uttar Pradesh</p>
-                       <p> This is the content of my first article. Hello my name is Aakanksha Malhotra.I am a Btech Student of 3rd year at JSS Academy of Technical Education,Noida ,Uttar Pradesh</p>`
-                   },       
-'article-two':{
-               title:'article two|Aakanksha Malhotra',
-               heading:'article-two',
-               date:November 2016,
-               content:`<p> This is the content of my second article. Hello my name is Aakanksha Malhotra.I am a Btech Student of         3rd year at JSS Academy of Technical Education,Noida ,Uttar Pradesh</p>
-                        <p> This is the content of my second article. Hello my name is Aakanksha Malhotra.I am a Btech Student of 3rd year at JSS Academy of Technical Education,Noida ,Uttar Pradesh</p>
-                         <p> This is the content of my second article. Hello my name is Aakanksha Malhotra.I am a Btech Student of 3rd year at JSS Academy of Technical Education,Noida ,Uttar Pradesh</p>`
-    
-              },
-'article-three':{
-    title:'article three|Aakanksha Malhotra',
-    heading:'article-three',
-    date:November15 2016,
-    content:`<p> This is the content of my third article. Hello my name is Aakanksha Malhotra.I am a Btech Student of 3rd year at JSS Academy of Technical Education,Noida ,Uttar Pradesh</p>
-          <p> This is the content of my third article. Hello my name is Aakanksha Malhotra.I am a Btech Student of 3rd year at JSS Academy of Technical Education,Noida ,Uttar Pradesh</p>
-          <p> This is the content of my third article. Hello my name is Aakanksha Malhotra.I am a Btech Student of 3rd year at JSS Academy of Technical Education,Noida ,Uttar Pradesh</p>`}
-};
-function createTemplate(data){
-    var title=data.title;
-    var heading=data.heading;
-    var date=data.date;
-    var contents=data.contents;
-}
-var htmlTemplate=`
-<html>
-<head>
-    <title> 
-         ${title}
-    </title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="/ui/style.css" rel="stylesheet" />
-  </head>
-  <body>
-      <div>
-          <a href="\">Home</a>
-                ${heading}
-      </div>
-      <hr/>
-      <div>
-             ${date}
-      </div>
-      <div>
-             ${contents}
-          
-      </div>
-      </body>
-</html>
-`;
-return htmlTemplate;
-}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+
+
 app.get('/:article-Name',function(req,res){
     var articleName=req.params.articleName;
 res.send(createTemplate(articles[articleName]));
