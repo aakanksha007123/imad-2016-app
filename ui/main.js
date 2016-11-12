27439a8322561ca8303ce3d6d3346c059dbdcb57
aakanksha007123 <aakanksha007123@gmail.com>
2016-11-12 20:34:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,12 @@ element.innerHTML='New Value';
 
 // move the image
 var img=document.getElementById("madi");
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function(){
+    var interval=setInterval(moveRight,100);
     
-    img.style.marginLeft='100px';
 }
