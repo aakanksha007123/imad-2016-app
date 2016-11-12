e20322b28cdd8ab7aee7fddf616774f97290cb16
aakanksha007123 <aakanksha007123@gmail.com>
2016-11-12 20:28:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,11 @@
 console.log('Loaded!');
+
 // change the text of main-text div
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
+
+// move the image
+var img=document.getElementById('img');
+img.onclick=function(){
+    img.style.marginLeft='100px';
+}
