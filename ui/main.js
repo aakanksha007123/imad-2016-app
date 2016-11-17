80c2d84a1f4b2c2d280490d5f8aee52a93b5b68a
aakanksha007123 <aakanksha007123@gmail.com>
2016-11-17 22:08:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,21 +3,20 @@ var button=document.getElementById('counter');
 
 button.onClick=function(){
   //create a request to the counter endpoint
-  var request=new XMLHTTPRequest();
+  var request=new XMLHttpRequest();
   //capture the response and store it in the variable
   request.onreadystatechange=function(){
-      if(request.readyState==XMLHTTPRequest.DONE){
+      if(request.readyState==XMLHttpRequest.DONE){
           //take action
           if(request.status==200){
               var counter=request.responseText;
-              names=JSON.parse(names);
+              
              var span=document.getElementById('counter');
              span.innerHTML=counter.toString();  
           }
       }
       // not done yet
-      
-  };
+      };
   //Make a request
   request.open('GET','http://aakanksha007123.imad.hasura-app.io/counter',true);
   request.send(null);
@@ -27,9 +26,9 @@ button.onClick=function(){
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
 	var name=nameInput.value;
-	var request=new XMLHTTPRequest();
+	var request=new XMLHttpRequest();
 	request.onreadystatechange=function(){
-	  if(request.readyState==XMLHTTPRequest.DONE){
+	  if(request.readyState==XMLHttpRequest.DONE){
 		  if(request.status==200){
 			var namesList=request.responseText;
 			names=JSON.parse(namesList);
@@ -44,7 +43,5 @@ submit.onclick=function(){
 	  }
 	}
 	};
-
-	request.open('GET','http://aakanksha007123.imad.hasura-app.io/submit-one?name='+name,true);
-	request.send('null');
-	request.send(null);
+request.open('GET','http://aakanksha007123.imad.hasura-app.io/submit-one?name='+name,true);
+request.send(null);
