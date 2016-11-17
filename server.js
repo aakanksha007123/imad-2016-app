6430b7cdfc8c0fd62edb4b9a59d9149a88e6d7b4
aakanksha007123 <aakanksha007123@gmail.com>
2016-11-17 23:12:10 +0530

    [imad-console] Updates server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+var express=require('express');
+var morgan=require('morgan');
+var path=require('path');
+
+var app=express();
+app.use(morgan('combined'));
+
+app.get('/',function(req,res){
+   res.sendFile(path(__dirname,'ui','index.html'));
+});
+app.get('/article-one',function(req,res){
+   res.sendFile(path(__dirname,'ui','article-one.html'));
+});
+app.get('/article-two',function(req,res){
+   res.sendFile(path(__dirname,'ui','article-two.html'));
+});
+app.get('/article-three',function(req,res){
+   res.sendFile(path(__dirname,'ui','article-three.html'));
+});
+app.get('/ui/style.css',function(req,res){
+   res.sendFile(path(__dirname,'ui','style.css'));
+});
+app.get('/ui/madi.png',function(req,res){
+   res.sendFile(path(__dirname,'ui','madi.png'));
+});
