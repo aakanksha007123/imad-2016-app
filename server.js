7488addbfdacd4a6d669ffca6375b90b105c02a1
aakanksha007123 <aakanksha007123@gmail.com>
2016-11-18 19:52:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var config = {
     database: 'coco98',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.env.DB_PASSWORD
+    password: process.
 };
 
 var app = express();
