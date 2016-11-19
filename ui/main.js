c3cbc944e33278c58a4258f11a19dac67693f710
aakanksha007123 <aakanksha007123@gmail.com>
2016-11-19 19:18:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,12 @@
-function loadLoginForm () {
-    var loginHtml = `
+var loginHtml = `
         <h3>Login/Register to unlock awesome features</h3>
-        <input type="text" id="username" placeholder="username" />
-        <input type="password" id="password" />
-        <br/><br/>
-        <input type="submit" id="login_btn" value="Login" />
-        <input type="submit" id="register_btn" value="Register" />
+	        Username: <input type="text" id="username" placeholder="Username" /><br/><br/>
+    	    Password:&nbsp;&nbsp;<input type="password" id="password" />
+        	<br/><br/>
+			<div style="padding-left: 100px;">
+		        <input type="submit" id="login_btn" value="Login" />
+    		    <input type="submit" id="register_btn" value="Register" />
+			</div>
         `;
     document.getElementById('login_area').innerHTML = loginHtml;
     
