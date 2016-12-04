c6d46b7315bbd49df9c5e1b1adfd25af05e68ec6
aakanksha007123 <aakanksha007123@gmail.com>
2016-12-04 17:22:01 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -12,7 +12,7 @@ function loadCommentForm () {
     document.getElementById('comment_form').innerHTML = commentFormHtml;
     
     // Submit username/password to login
-    var submit = document.getElementById('submit');
+    var submit = document.getElementById('submit_comment');
     submit.onclick = function () {
         // Create a request object
         var request = new XMLHttpRequest();
